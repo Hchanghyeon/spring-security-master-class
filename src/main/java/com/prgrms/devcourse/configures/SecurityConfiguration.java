fe08494eceee77af6d81cc0384e133d132d74027
changhyeon <changhyeon.h@kakao.com>
2023-08-07 11:09:27 +0900

    feat: h2 데이터베이스 설정

--- src/main/java/com/prgrms/devcourse/configures/SecurityConfiguration.java
+++ src/main/java/com/prgrms/devcourse/configures/SecurityConfiguration.java
@@ -3,21 +3,25 @@ package com.prgrms.devcourse.configures;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration {
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/me")).hasAnyRole("USER", "ADMIN")
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
@@ -33,6 +37,9 @@ public class SecurityConfiguration {
                 )
                 .requiresChannel(channel -> channel
                         .anyRequest().requiresSecure()
+                )
+                .headers(headers -> headers
+                        .frameOptions(FrameOptionsConfig::sameOrigin)
                 );
 
         return http.build();
